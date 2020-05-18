91b099d146725cc6cfe4a6aa1d14408aadf1c1de
Joke Heyndels <jo_hannah@outlook.com>
2020-05-18 18:36:44 +0200

    Add Course model in education app

---
+++ education/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    """ Save course info """
+    name = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    school_url = models.URLField()
+    certificate = models.BooleanField()
+    certificate_image = models.URLField(null=True, blank=True)
+    date_finished = models.DateField()
+    description = models.TextField()
