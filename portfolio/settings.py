7b55f1fef7ae6484435401a590eda59acae3b05c
Joke Heyndels <jo_hannah@outlook.com>
2020-05-23 17:14:04 +0200

    Make staticfiles folder instead of static

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
