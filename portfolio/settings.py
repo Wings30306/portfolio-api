e6ff3cda1bd45367e3d63f82e0f5f6d634394ee0
Joke Heyndels <jo_hannah@outlook.com>
2020-06-09 21:15:11 +0200

    Add default trottle rate to prevent error on fetch

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -141,5 +141,13 @@ STATIC_ROOT = 'staticfiles'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/hour',
+        'user': '1000/hour'
+    }
 }
